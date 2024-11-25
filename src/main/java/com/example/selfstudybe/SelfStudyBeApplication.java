004d79a0901c8ccafbb3a6a160e962218a7b2e0a
Test_repo <22520992@gm.uit.edu.vn>
2024-11-25 16:59:46 +0700

    Initial project

---
+++ src/main/java/com/example/selfstudybe/SelfStudyBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.selfstudybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfStudyBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfStudyBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/selfstudybe/SelfStudyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.selfstudybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfStudyBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
