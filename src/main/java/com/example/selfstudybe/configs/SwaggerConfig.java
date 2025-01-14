355193cd57e5759dfa8c9a25bcd4005f998ae4f4
Test_repo <22520992@gm.uit.edu.vn>
2025-01-14 12:44:00 +0700

    Fix swagger request url

--- src/main/java/com/example/selfstudybe/configs/SwaggerConfig.java
+++ src/main/java/com/example/selfstudybe/configs/SwaggerConfig.java
@@ -3,18 +3,26 @@ package com.example.selfstudybe.configs;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.servers.Server;
+
+import java.util.Collections;
 
 @Configuration
 public class SwaggerConfig {
+    @Value("${openapi.base-url}")
+    private String baseUrl;
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                    .title("StudyPal API")
-                    .description("API for self-study mobile application.")
-                    .version("1.0"));
+                        .title("Self Study API")
+                        .version("1.0")
+                        .description("API documentation for Self Study application"))
+                .servers(Collections.singletonList(new Server().url(baseUrl)));
     }
 
     @Bean
